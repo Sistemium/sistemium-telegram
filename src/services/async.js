bdbdbdb651313f73e558a25eef2436e83b92f112
Alexander Levin <alevin.ru@gmail.com>
2018-09-29 11:19:53 +0300

    filterSeries

--- src/services/async.js
+++ src/services/async.js
@@ -1,27 +1,25 @@
 import eachSeries from 'async/eachSeries';
 import whilst from 'async/whilst';
+import filterSeries from 'async/filterSeries';
 
-export async function eachSeriesAsync(arr, fn) {
-
-  return new Promise((resolve, reject) => {
-
-    eachSeries(arr, fn, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-
-  });
-
+export async function eachSeriesAsync(arr, iterator) {
+  return applyAsync(eachSeries, arr, iterator);
 }
 
-export async function whilstAsync(filter, fn) {
+export async function whilstAsync(filter, iterator) {
+  return applyAsync(whilst, filter, iterator);
+}
+
+
+export async function filterSeriesAsync(arr, iterator) {
+  return applyAsync(filterSeries, arr, iterator);
+}
+
+function applyAsync(asyncFn, arg1, arg2) {
 
   return new Promise((resolve, reject) => {
 
-    whilst(filter, fn, (err, data) => {
+    asyncFn(arg1, arg2, (err, data) => {
       if (err) {
         reject(err);
       } else {
