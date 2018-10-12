6ff36ace0f7144b3beb3f22238cc846934f57927
Alexander Levin <alevin.ru@gmail.com>
2018-10-12 18:06:47 +0300

    auth service

---
+++ src/services/auth.js
@@ -0,0 +1,42 @@
+import http from 'axios';
+
+const PHA_ROLES_URL = 'https://api.sistemium.com/pha/roles';
+const PHA_AUTH_URL = 'https://api.sistemium.com/pha/auth';
+
+export function roles(token) {
+
+  return http.get(PHA_ROLES_URL, {
+    headers: { authorization: token },
+  })
+    .then(res => res.data);
+
+}
+
+export function login(phone) {
+
+  const config = {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  };
+
+  return http.post(PHA_AUTH_URL, `mobileNumber=${phone}`, config)
+    .then(res => res.data.ID);
+
+}
+
+export async function confirm(code, id) {
+
+  const params = { ID: id, smsCode: code };
+  const config = {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    transformRequest: [data => {
+      const str = Object.keys(data)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`);
+      return str.join('&');
+    }],
+  };
+
+  const { data } = await http.post(PHA_AUTH_URL, params, config);
+
+  return data;
+
+}
