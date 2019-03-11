223c7c9be5960c8c61af958c1056ff3a30cf34a7
Alexander Levin <alevin.ru@gmail.com>
2019-03-11 13:08:02 +0200

    mapSeriesAsync

--- src/services/async.js
+++ src/services/async.js
@@ -1,6 +1,7 @@
 import eachSeries from 'async/eachSeries';
 import whilst from 'async/whilst';
 import filterSeries from 'async/filterSeries';
+import mapSeries from 'async/mapSeries';
 
 export async function eachSeriesAsync(arr, iterator) {
   return applyAsync(eachSeries, arr, iterator);
@@ -13,6 +14,10 @@ export async function whilstAsync(filter, iterator) {
 
 export async function filterSeriesAsync(arr, iterator) {
   return applyAsync(filterSeries, arr, iterator);
+}
+
+export async function mapSeriesAsync(arr, iterator) {
+  return applyAsync(mapSeries, arr, iterator);
 }
 
 function applyAsync(asyncFn, arg1, arg2) {
